53974d589c688b2e48734114b5794a622d0069f1
R* <64038390+raster0x2a@users.noreply.github.com>
2020-12-03 17:57:30 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord 
+
+client = discord.Client()
+@client.event
+async def on_ready(): 
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.author.bot:
+        return
+
+    if message.content == "# ping":
+        await message.channel.send("pong")
+
+if __name__ == "__main__":
+    token = os.environ['DISCODEBOT_TOKEN']
+    client.run(token)
